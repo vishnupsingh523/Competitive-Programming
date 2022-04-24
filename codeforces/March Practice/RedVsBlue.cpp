1b452a9413e04e14e2805eb3449b6d4cdba69e9d
Vishwanath Pratap Singh <vishnupsingh523@gmail.com>
2022-04-24 23:26:57 +0530

    fiels added

--- codeforces/March Practice/RedVsBlue.cpp
+++ codeforces/March Practice/RedVsBlue.cpp
@@ -8,91 +8,24 @@ int main(){
     int t;
     cin>>t;
     while(t--){
+
         int n, r, b;
         cin>>n>>r>>b;
+
         string ans = "";
 
-        int diff = r-b;
-        
-        if(diff>0)
-        {
-            if(diff>n/2)
-            {
-                diff = n-diff;
-                int count =0;
 
-                for(int i=0;i<n;i++)
-                {
-                    if(count<diff)
-                    {
-                        ans+='R';
-                        count++;
-                    }
-                    else{
-                        count=0;
-                        ans+='B';
-                    }
-                }
-            }
-            else
-            {
-                int count =0;
+        int diff = r/(b+1);
+        int mod = r%(b+1);
 
-                for(int i=0;i<n;i++)
-                {
-                    if(count<diff)
-                    {
-                        ans+='R';
-                        count++;
-                    }
-                    else{
-                        count=0;
-                        ans+='B';
-                    }
-                }
-            }
-        }
-        else
-        {
-            diff = -(diff);
-            if(diff>n/2)
-            {
-                diff = n-diff;
-                int count =0;
+        for(int i=0;i<mod; i++)
+        cout<<string(diff+1,'R')<<'B';
 
-                for(int i=0;i<n;i++)
-                {
-                    if(count<diff)
-                    {
-                        ans+='B';
-                        count++;
-                    }
-                    else{
-                        count=0;
-                        ans+='R';
-                    }
-                }
-            }
-            else
-            {
-                int count =0;
+        for(int i= mod; i<b;i++)
+        cout<<string(diff, 'R')<<'B';
 
-                for(int i=0;i<n;i++)
-                {
-                    if(count<diff)
-                    {
-                        ans+='B';
-                        count++;
-                    }
-                    else{
-                        count=0;
-                        ans+='R';
-                    }
-                }
-            }
-        }
-
-        cout<<ans<<endl;
+        cout<<string(diff, 'R');
+        cout<<endl;
     }
 
 }
